a3fd4730b7ee015975b9e019c41acabb9ee7cd1d
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-11-19 17:21:36 +0530

    go program :zap:

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 import "gofr.dev/pkg/gofr"
 //Add your own config folder , i have removed mine :)
+const payload = "Trying go for the first time !";
  func main(){
 	app := gofr.New();
 	app.GET("/ping", func(c *gofr.Context) (interface{}, error) {
 		return "PONG" , nil
-
-	})
+   })
+   app.PUT("/data",func(c *gofr.Context) (interface{}, error) {
+	return payload,nil
+   })
 app.Run()
  }
 
