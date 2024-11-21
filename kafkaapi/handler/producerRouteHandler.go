aa0bfdadc130ea8da48aa7b82658cda68dd4c808
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-11-21 10:20:11 +0530

    added producer handler :zap:

--- kafkaapi/handler/producerRouteHandler.go
+++ kafkaapi/handler/producerRouteHandler.go
@@ -1,4 +1,30 @@
 package handler
 import(
-	
+	"gofr-server/kafkaapi/producer"
+	"log"
+	"gofr.dev/pkg/gofr"
 )
+
+type ProduceRequest struct {
+	Broker  string `json:"broker"`
+	Topic   string `json:"topic"`
+	CSVFile string `json:"csvFile"`
+}
+
+func ProducerHandler(c *gofr.Context) string {
+		var req ProduceRequest
+		if err := c.Bind(&req); err != nil {
+			errormsg := "Error in payload"
+			return errormsg
+		}
+		config := producer.ProducerConfig{
+			Broker: req.Broker,
+			Topic:  req.Topic,
+		}
+		go func() {
+			if err := producer.KafkaProducer(config, req.CSVFile); err != nil {
+				log.Printf("Failed to produce messages: %v", err)
+			}
+		}()
+	      return "Message entered successfully :)"
+	}
